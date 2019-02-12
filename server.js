d1dfad70393bcf806d1828a09817b9b38cb95c8f
ajzbc <contact.ajzbc@gmail.com>
2019-02-11 23:33:24 -0800

    ports setup wrong i think

--- server.js
+++ server.js
@@ -57,7 +57,9 @@ app.get('/', function (req, res) {
     });
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 function incrementAPICalls() {
     var apiCalls = publicStats.get()
